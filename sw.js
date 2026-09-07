811e1be1eaf12351ef3b6150cee0ec72a7df1bbe
swagtop411-max <swagtop411@gmail.com>
2026-09-07 00:56:56 -0300

    chore: atualiza cache para beta espelhada V20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="bd-atletas-v19-20260907-1";
+const CACHE_NAME="bd-atletas-v20-20260907-1";
 const CORE=["/","/index.html","/site-theme.css","/site-v8.css","/site-v5.js","/site-v8.js","/media-utils.js","/public-beta-v19.js","/manifest.webmanifest","/teste-social.html","/atletas.html","/comunidade.html","/proximos-campeonatos.html","/conta.html"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME&&key.startsWith("bd-atletas-")).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
