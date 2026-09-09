8f8e4a8ec846fbd279701cba09d5966094feeaa6
swagtop411-max <swagtop411@gmail.com>
2026-09-09 14:55:06 -0300

    fix: use secure story visibility handler

--- avatar-story-v36.js
+++ avatar-story-v36.js
@@ -117,4 +117,4 @@ if (avatar && uid) {
   setTimeout(updateAvatarState, 1200);
 }
 
-import("./profile-highlights-v37.js?v=20260908-37").catch(error => console.warn("Destaques V37:", error));
+import("./profile-story-access-v45.js?v=20260909-45").catch(error => console.warn("Stories públicos V45:", error));
